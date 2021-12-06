518082d60412cfdd37d703e64b106a6115c79a4b
ahuszar <huszar.alexandru@gmail.com>
2021-12-06 14:46:21 +0200

    Explaining arrow functions

--- firstFile.js
+++ firstFile.js
@@ -1,3 +1,39 @@
-const some=() => {
+
+function sum1() {
+    //code
+    this // this din functie
+
+}
+
+const some = () => {
+    // this asta e din afara functie
+    this
+}
+
+const sum = () => {
+
+    return () => {
+        a+b
+        // this asta e din afara functiilor
+        this
+    }
+}
+
+const plus = () => () => {
+    a+b
+    //
+    this
+}
+
+
+
+class Auto {
+    constructor() {
+    }
+
+    addVim = () => {
+        this.name = 'xxx'
+    }
+
 
 }
