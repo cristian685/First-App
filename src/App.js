2fbfa2a84cf6c48cd6514a7865acef77f98a0223
cristian685 <cristi.popina98@gmail.com>
2021-12-08 17:21:01 +0200

    first app commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from "./views/Login";
+import React from "react";
+import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
+import ForgotPassword from "./components/ForgotPassword";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       Hello wordl
-      </header>
-    </div>
+   return (
+       <Router>
+           <div className="App">
+               <Login />
+               <Switch>
+                   <Route exact path='/forgotPass' element={< ForgotPassword />}>
+                   </Route>
+               </Switch>
+           </div>
+       </Router>
   );
-}
-
-export default App;
+ }
+ export default App
 
---
+++ src/components/Title/Subtitle.js
@@ -0,0 +1,5 @@
+import React from 'react';
+export default function Subtitle(props) {
+    const { subtitle = '' } = props;
+    return <div style={{backgroundColor: 'red', height: 30}}>{subtitle}</div>
+}
